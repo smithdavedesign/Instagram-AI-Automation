31e9ccaebb864d0e36da0e2cddcc36024bd6f3d7
smithdavedesign <1426dave@gmail.com>
2024-08-01 10:29:37 -0700

    backbone of project ready

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require("express-session");
+const dotenv = require('dotenv');
+const path = require('path');
+const instagramControllers = require('./controllers/instagramController');
 
-var instagramControllers = require('./controllers/instagramControllers');
-var cronjobs = require('./controllers/cronJobs');
+dotenv.config();
 
-var app = express();
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const init = async () => {
+  try {
+    // Session setup
+    app.use(
+      session({
+        secret: "dave-is-cool",
+        resave: true,
+        saveUninitialized: true,
+      })
+    );
 
-app.use('/', instagramControllers);
-app.use('/cronjobs', cronjobs);
+    // Middleware setup
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
-module.exports = app;
+    // Routes setup
+    app.use('/', instagramControllers);
+    // app.use('/cronjobs', cronJobs);
+
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`App listening on ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Server initialization failed:", error.message);
+    console.error(error.stack);
+  }
+};
+
+init();
+
+module.exports = { app };
--- controllers/instagramController.js
+++ controllers/instagramController.js
@@ -1,6 +1,11 @@
 // Import necessary modules or libraries
 var express = require('express');
+var router = express.Router();
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send('helloworld');
+});
 
 // Define your controller functions
 
